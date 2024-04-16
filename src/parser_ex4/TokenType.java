304bb65ee48eb390eccbe95e5e03b7a105abf0cd
SOTIRIS CHARAMIDIS <65503180+SotirisXaram@users.noreply.github.com>
2024-04-16 20:29:52 +0300

    tokenType fix

--- src/parser_ex4/TokenType.java
+++ src/parser_ex4/TokenType.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package parser_ex4;
 
 
@@ -13,11 +9,16 @@ public enum TokenType {
     whitespaceTK("[ \t\f\r]+"),
     newlineTK("\n"),
     commentTK("\\{[^{}\\n]*\\}"),
+  
+
+
 //-------------------------------------------------------------------
 // Constants
 //-------------------------------------------------------------------    
     numericTK("-?\\d+"),
     stringConstTK("\"([^\"\\\\\\n]|\\\\[\"\\\\])*\""),
+  //stringConstTK("\"(?:\\\\\"|\\\\\\\\|[^\"\\\\\\n])*\"")
+
 //-------------------------------------------------------------------
 // Keywords
 //-------------------------------------------------------------------
