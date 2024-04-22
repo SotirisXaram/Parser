3fbfed04ed602c666460f63d05aff38de5a39c84
SOTIRIS CHARAMIDIS <65503180+SotirisXaram@users.noreply.github.com>
2024-04-22 20:15:32 +0300

    Update Token.java

--- src/parser_ex4/Token.java
+++ src/parser_ex4/Token.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package parser_ex4;
 
 public class Token {
